4f71ee93ed401bf23a93de9581d7ff0140d4abb5
massercapper67 <alvarobardajirobles@gmail.com>
2025-10-18 17:26:14 +0200

    Added base structure of cpu.h and added more code to emu.c

--- gbemu/emu.c
+++ gbemu/emu.c
@@ -18,6 +18,9 @@
 
 
 static emu_context context;
+static char *emu_command = "cboy";
+static int COMMAND_ERROR = -1;
+static int ROM_ERROR = -2;
 
 emu_context *emu_get_context() {
   return &context;
@@ -28,5 +31,22 @@ void delay(u32 ms) {
 }
 
 int emu_run(int argc, char **argv) {
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s <rom_file>\n", emu_command);
+    return COMMAND_ERROR;
+  }
 
+  if (!cartridge_load(argv[1])) {
+    fprintf(stderr, "Failed to load ROM file: %s\n", argv[1]);
+    return ROM_ERROR;
+  }
+
+  printf("Cart loaded...\n");
+
+  SDL_Init(SDL_INIT_VIDEO);
+  printf("SDL INIT\n");
+  TTF_Init();
+  printf("TTF INIT\n");
+
+  cpu_init();
 }
---
+++ include/cpu.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <common.h>
+
+void cpu_start();
+bool cpu_step();
